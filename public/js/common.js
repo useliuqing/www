d9f27ec37a31d334af5b2b55f3ff99c5b6468a23
liuqing <useliuqing@gmail.com>
2017-01-03 23:55:15 +0900

    商品注册
    商品注册功能完善

--- public/js/common.js
+++ public/js/common.js
@@ -8,7 +8,7 @@ var product = (function ($) {
 
     };
 
-    　　//商品详细相关　
+    //商品详细相关　
     p.item = {
         //注册新商品
         setNewProduct: setNewProduct,
@@ -19,22 +19,23 @@ var product = (function ($) {
     };
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+    
     function setViewableInfo() {
         window.location.href = "/setViewableInfo";
     }
 
-
+    var newProductsNumber = 0;
     function setNewProduct() {
         //开始注册商品
-        if ($(event.target).children().hasClass('glyphicon-plus')) {
-            var jdiv = $('<div></div>');
-            jdiv.append('<table class="table table-striped"></table>')
-                .children('table')
+        if ($(event.target).parent().find('.glyphicon-plus').length ==1) {
+            var jdiv = $('<div><form method="post"><fieldset></fieldset></form></div>');
+            jdiv.find('fieldset')
+                .append('<table class="table table-striped"></table>')
+                .children()
                 .append('<thead><tr></tr></thead>')
                 .find('tr')
-                .append('<th>商品名称</th>')
                 .append('<th>商品图片</th>')
+                .append('<th>商品名称</th>')
                 .append('<th>价格</th>')
                 .append('<th>种类</th>')
                 .append('<th>供应商</th>')
@@ -42,31 +43,65 @@ var product = (function ($) {
                 .append('<th>产地</th>')
                 .append('<th><th>')
                 .parent()
-                .after('<tbody><tr class="warning"></tr></tbody>')
+                .after('<tbody id="newItemsInfo"><tr class="success"></tr></tbody>')
                 .next()
                 .children('tr')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
-                .append('<td><input type="text"></td>')
+                .append('<td><input type="file" name="picture' + newProductsNumber + '"></td>')
+                .append('<td><input type="text" name="productName'+ newProductsNumber +'"></td>')
+                .append('<td><input type="text" name="price' + newProductsNumber +'"></td>')
+                .append('<td><input type="text" name="kind"></td>')
+                .append('<td><input type="text" name="supplier"></td>')
+                .append('<td><input type="text" name="procurementPrice"></td>')
+                .append('<td><input type="text" name="producingArea"></td>')
                 .append('<td><span class="glyphicon glyphicon-plus" onclick="product.item.addProduct()"></span></td>')
-                .find('input').css('width', 80);
+                .find('input').css('width', 100);
             jdiv.appendTo('#itemPlusHead');
-            $('#itemPlusHead').children('h3').find('.glyphicon').removeClass('glyphicon-plus').addClass('glyphicon-ok').text('确认');
+            newProductsNumber++;
+            $('#itemPlusHead').children('h3').find('.glyphicon').removeClass('glyphicon-plus').addClass('glyphicon-ok').text("确认");
         } else {
             //商品注册结束
-
+            var jtrs = $("#itemsInfo").find('tr');
+            var newJtrs = $('#newItemsInfo').find('tr');//注册的新商品的tr
+            var names = [], newNames = [];//存入所有既存商品的名字
+            var flag = false;
+            jtrs.each(function (i, tr) {
+                names.push(tr.children().eq(2).text());
+            });
+            newJtrs.each(function (i, tr) {
+                newNames.push(tr.children[1].children[0].value);
+            });
+            //检测重复
+            newNames.every(function(newName){
+                if(names.indexOf(newName) != -1){
+                    alert("商品"+ newName +"已存在");
+                    flag = true;
+                    return false;
+                }
+            });
+            if(!flag){
+                $('fieldset').append('<input type="text" name="number">')
+                             .children('input[name="number"]')
+                             .attr('value',newProductsNumber)
+                             .parent().parent()
+                             .submit();
+            }
         }
     }
 
     function addProduct() {
         //检测填入的信息
-
-        var jtrCopy = $(event.target).parent().parent().clone();
-        $(event.target).parent().empty().parent().after(jtrCopy);
+        var jtr = $('<tr class="success"></tr>');
+        jtr.append('<td><input type="file" name="picture"></td>')
+            .append('<td><input type="text" name="productName"></td>')
+            .append('<td><input type="text" name="price"></td>')
+            .append('<td><input type="text" name="kind"></td>')
+            .append('<td><input type="text" name="supplier"></td>')
+            .append('<td><input type="text" name="procurementPrice"></td>')
+            .append('<td><input type="text" name="producingArea"></td>')
+            .append('<td><span class="glyphicon glyphicon-plus" onclick="product.item.addProduct()"></span></td>')
+            .find('input').css('width', 100);
+        newProductsNumber++;
+        $(event.target).parent().empty().parent().after(jtr);
     }
 
     return p;
--- routes/item.js
+++ routes/item.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+var path = require('path');
 var checkLogin = require('../middlewares/check').checkLogin;
 var ItemDao = require('../models/ItemDao');
 //商品编辑页面
@@ -10,13 +11,36 @@ router.get('/',checkLogin,function(req,res,next){
     })
 });
 
+//新商品注册
+router.post('/',function(req,res,next){
+    var fields  = req.fields;
+    var items = [];
+    for(var i = 0; i < fields.number;i++){
+        var item = {};
+        item.productName = fields.productName[i];
+        item.price = fields.price[i];
+        item.kind = fields.kind[i];
+        item.supplier = fields.supplier[i];
+        item.procurementPrice = fields.procurementPrice[i];
+        item.producingArea = fields.producingArea[i];
+        item.picture = files.picture[i].path.split(path.sep).pop();
+        items.push(item);
+    }
+
+    ItemDao.create(items)
+           .then(function(results){
+               res.redirect('/item');
+           }).catch(function(e){
+               next(e);
+           })
+
+});
 
 function getAllItemsInfo(userName){
-    var p = new Promise(function(resolve,reject){
+    return new Promise(function(resolve,reject){
         var items = ItemDao.getItems(userName);
         resolve(items);
     });
-    return p;
 }
 
 module.exports = router;
